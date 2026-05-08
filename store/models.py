1abcb7a3c5ad4064466969d7908a59d7ab98c527
Gutierrez-Jostin <elenamarquez2000casilla@gmail.com>
2026-05-08 16:06:02 -0700

    GUardar cambios

--- store/models.py
+++ store/models.py
@@ -74,6 +74,10 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"Cart {self.id} - {self.user}"
+    # Actualizar    
+    @property
+    def total(self):
+        return sum(item.subtotal for item in self.cartitem_set.all())
 
 
 # =========================
@@ -92,3 +96,10 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.product} x {self.quantity}"
+    # Actualizar
+    @property
+    def subtotal(self):
+            return self.product.price * self.quantity
+
+    def __str__(self):
+            return f"{self.product} x {self.quantity}"
